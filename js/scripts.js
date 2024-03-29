c91a7048b85595f3487b5ffc1dc5ace18870d63c
Jordon Draper <jordondraper17@gmail.com>
2024-03-28 23:40:55 -0600

    let's check it out

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+});
